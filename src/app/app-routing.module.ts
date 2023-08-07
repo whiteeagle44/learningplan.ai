144ecdfd68608b168c84523efd0775198e168975
Marcin Jankowski <marcin.jankowski@talan.com>
2023-08-07 16:11:54 +0200

    create plan form, plan service and openai service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePlanComponent } from './create-plan/create-plan.component';
+import { PlanComponent } from './plan/plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CreatePlanComponent },
+  { path: 'checkout', component: PlanComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePlanComponent } from './create-plan/create-plan.component';
+import { PlanComponent } from './plan/plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePlanComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
